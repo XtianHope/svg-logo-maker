75bfbbd61b95ca2c893b97a71aba47264a037f94
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-08 20:45:02 -0500

    added const generate shape to pull data from shapes.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Importing needed modules: 'inquirer' for user prompts, 'fs' for file handling.
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { generateShape } = require('./lib/shapes');
 
 
 // Function to create a logo based on user input.
