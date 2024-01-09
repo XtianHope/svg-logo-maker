6d753701252a7751a14752282765528d0afc781d
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-08 19:57:04 -0500

    added prompt for user to select text, text color, shape choice, and shape color to create a logo function

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ const fs = require('fs');
 
 // Function to create a logo based on user input.
 function createLogo() {
-    
+// Prompt for users for text, text color, shape choice, and shape color.
+inquirer
+.prompt([
+  { type: 'input', name: 'text', message: 'Enter up to three characters for the text:' },
+  { type: 'input', name: 'textColor', message: 'Enter text color:' },
+  { type: 'list', name: 'shapeChoice', message: 'Choose a shape:', choices: ['circle', 'triangle', 'square'] },
+  { type: 'input', name: 'shapeColor', message: 'Enter shape color:' },
+])
+
 }
