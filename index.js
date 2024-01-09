9b905665bf64fa85906d318abb89d4399c25dc5e
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-08 19:54:52 -0500

    beginning function to generate a logo

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // Importing needed modules: 'inquirer' for user prompts, 'fs' for file handling.
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+// Function to create a logo based on user input.
+function createLogo() {
+    
+}
